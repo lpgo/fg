9063df99aabf083e41e3356340f091c6e32b10ec
lpgo <whereisgirl@126.com>
2016-04-27 21:57:23 +0800

    a

--- src/wx.rs
+++ src/wx.rs
@@ -434,10 +434,8 @@ pub fn get_hot_lines(req: &mut Request) -> IronResult<Response> {
 }
 
 pub fn test(req: &mut Request) -> IronResult<Response> {
-    let data = model::make_data();
-    let mut resp = Response::new();
     pay::send_sms();
-    res_template!("index",data,resp)
+    Ok(Response::with(status::Ok))
 }
 
 pub fn ico(req: &mut Request) -> IronResult<Response> {
