d6d7967117e986052d534369c22325d4545955c8
lpgo <whereisgirl@126.com>
2016-04-29 16:47:27 +0800

    a

--- static/js/app.js
+++ static/js/app.js
@@ -68,6 +68,34 @@ app.controller('BuySeatCtrl', ['$scope','$routeParams','$http', function($scope,
     });
   }
   getTripDetail(oid);
+
+  function pay(data) {
+    WeixinJSBridge.invoke(
+       'getBrandWCPayRequest', 
+       {
+          "appId" :data.appId,     
+           "timeStamp":data.timeStamp,         //时间戳，自1970年以来的秒数     
+           "nonceStr":data.nonceStr, //随机串     
+           "package":data.package,     
+           "signType":"MD5",         //微信签名方式：     
+           "paySign":data.paySign //微信签名 
+       },
+       function(res){     
+           if(res.err_msg == "get_brand_wcpay_request：ok" ) {
+                alert("ok");
+           } else {
+                alert(res.err_msg);
+           }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+       }
+    ); 
+  }
+
+  $scope.applyTrip = function() {
+    $http.post("applyTrip",{oid:oid}).success(function(data){
+      pay(data);
+    });
+  }
+
 }]);
 
 app.controller('ListCtrl', ['$scope','$location', function($scope,$location){
